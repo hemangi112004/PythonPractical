631a051283f081a445ebb1305fb826294a941530
Hemangi <rbprajapati2k8@gmail.com>
2024-07-22 12:41:24 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,58 @@
+"""age = int(input("Enter your age"))
+if(age>=18):
+    print("Can VOte")
+else:
+    print("Cant Vote")
+
+light = "Green"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "Green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+
+# comparision stmt
+marks = int(input("Enter Your marks : "))
+
+if(marks>=90):
+    print("You Got A ")
+elif(marks>=90 and marks<=80):
+    print("you Got B ")
+elif(marks>=80 and marks<=70):
+    print("you got C ")
+else:
+    print("You Got D ")"""
+
+
+#odd even 
+"""""
+no = int(input("Enter your no:  " ))
+
+if(no%2 == 0):
+    print("even")
+else:
+    print("odd")"""
+
+#greatest number find
+"""""
+no1 = int(input("Enter your First no : "))
+no2 = int(input("Enter your Second no : "))
+no3 = int(input("Enter your Third no : "))
+
+if(no1>no2):
+    print("no1 is biggest")
+elif(no2>no3):
+    print("no2 is biggest")
+else:
+    print("no3 is biggest")
+"""
+#check is it multiple of 7 or not
+no = int(input("Enter your no:  " ))
+
+if(no%7 == 0):
+    print("Multiple of 7")
+else:
+    print("not ")
+
